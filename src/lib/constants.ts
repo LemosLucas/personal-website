f978d998244694f31a93ca2e3b849cbe55dd159f
Lucas Lemos <guedeslemos29@gmail.com>
2023-09-29 14:21:45 -0300

    serve .pdf files from my domain
    - add my .pdf straight to the repo to better serve it
    - fix the project link to highlight there is something to click

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,3 +2,15 @@ export const LINKS = {
   github: "https://github.com/lemoslucas",
   linkedin: "https://www.linkedin.com/in/lemoslucas/",
 };
+
+/*
+Contains links to files stored elsewhere that I want to serve from 
+my domain
+*/
+export const RESOURCE_FILES = {
+  gics_essay_pt_br: "docs/GICS-Relatorio-Final-Lucas-Lemos.pdf",
+  gipsa_essay_fr: "docs/GIPSA-Rapport-Finale-Lucas-Lemos.pdf",
+  gipsa_presentation_fr: "https://lemoslucas.github.io/gipsa/soutenance.html",
+  tdk_essay_en_us: "docs/TDK-Final-Essay-Lucas-Lemos.pdf",
+  resume_en_us: "docs/Lucas-Lemos-Resume.pdf"
+}
